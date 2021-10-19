067d29bc97c705c10ba7972cf411bc964d9676c6
muhamad farroos <muhammadfarroos12@gmail.com>
2021-10-19 21:51:31 +0700

    final submission-1

--- app/src/main/java/com/farroos/mygithubuser/DetailActivity.kt
+++ app/src/main/java/com/farroos/mygithubuser/DetailActivity.kt
@@ -1,6 +1,5 @@
 package com.farroos.mygithubuser
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
